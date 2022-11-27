7f23655e894f45639cf26a6c8234259781705917
Vignesh Ravindran <vignesh.ravi2211@gmail.com>
2022-11-27 21:42:43 +0530

    moved timeStamp generation to client side

--- server/util.js
+++
@@ -1,9 +0,0 @@
-const getTimeString = (dateStr) => {
-  const dateObj = new Date(dateStr);
-  const hours = dateObj.getHours().toString().padStart(2, "0");
-  const minutes = dateObj.getMinutes().toString().padStart(2, "0");
-  const result = `${hours}:${minutes}`;
-  return result;
-};
-
-module.exports = { getTimeString };
